b9a1c1e499aa08f7de70703cdbbba2622301caf8
salubrious-pipe-1423 <digregoriothomas@yahoo.com>
2019-06-23 01:30:21 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -26,11 +26,12 @@ end
 
 def end_game
   # code #end_game here
-  
+   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
   # code #initial_round here
+  
 end
 
 def hit?
